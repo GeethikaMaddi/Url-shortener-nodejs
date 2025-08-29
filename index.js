9a26df2616e29e231eb1f1501e86f9c12050edc7
GeethikaMaddi <mgeethika490@gmail.com>
2025-08-29 19:17:14 +0530

    Changes for .env file

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-// const express = require('express');
-// const cors = require('cors');
-// const app = express();
-
-// // Basic Configuration
-// const port = process.env.PORT || 3000;
-
-// app.use(cors());
-
-// app.use('/public', express.static(`${process.cwd()}/public`));
-
-// app.get('/', function(req, res) {
-//   res.sendFile(process.cwd() + '/views/index.html');
-// });
-
-// // Your first API endpoint
-// app.get('/api/hello', function(req, res) {
-//   res.json({ greeting: 'hello API' });
-// });
-
-// app.listen(port, function() {
-//   console.log(`Listening on port ${port}`);
-// });
 
 import 'dotenv/config'; // Loads environment variables from .env
 import express from 'express';
@@ -30,6 +7,7 @@ import cors from 'cors';
 import dns from 'dns';
 import validUrl from 'valid-url';
 
+require("dotenv").config();
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
